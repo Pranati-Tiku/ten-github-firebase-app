8d96067679575427e167157fb56c73de5b4f7b07
Pranati Tikku <pranatitikku@gmail.com>
2023-07-08 13:24:18 +0530

    Conditional rendering on navbar

--- src/Layout.js/NavBar.js
+++ src/Layout.js/NavBar.js
@@ -1,29 +1,40 @@
 import React,{useContext,useState} from "react";
-import { Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,NavItem,NavbarText, Col } from "reactstrap";
+import { Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,NavItem,NavbarText } from "reactstrap";
 
 import { NavLink,Link } from "react-router-dom";
 
 import { UserContext } from "../context/UserContext";
 
 const NavBar=()=>{
+    const context=useContext(UserContext)
+    const[isOpen,setIsOpen]=useState(false)
+    const toggle=()=>setIsOpen(!isOpen)
 return(
     <Navbar color="info" light expand="md">
         <NavbarBrand>
             <Link to="/" style={{textDecoration:"none"}}className="text-white">GitFire App</Link>
         </NavbarBrand>
-        <NavbarToggler/>
-        <Collapse navbar>
-        <Nav className="ml-auto" navbar>
+        <NavbarText className="text-white">
+            {context.user?.email ?(context.user.email):("")}
+        </NavbarText>
+        <NavbarToggler onClick={toggle}/>
+        <Collapse isOpen={isOpen}  navbar>
+        <Nav className="ms-auto" navbar>
+            {context.user ? ( 
             <NavItem>
-                <NavLink className="text-white">SignUp</NavLink>
+                <NavLink to="/" style={{textDecoration:"none"}} className="text-white p-2">LogOut</NavLink>
+            </NavItem>
+            ):(
+            <>
+            <NavItem>
+                <NavLink to="/" style={{textDecoration:"none"}} className="text-white p-2">SignUp</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink className="text-white">SignUp</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink className="text-white">LogOut</NavLink>
-            </NavItem>
-        </Nav>
+                <NavLink to="/" style={{textDecoration:"none"}} className="text-white p-2">SignUp</NavLink>
+             </NavItem>
+            </>
+            )}
+       </Nav>
         </Collapse>
     </Navbar>)
 }
